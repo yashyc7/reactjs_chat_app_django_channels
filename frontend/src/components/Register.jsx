0676d0b630c6a34d9c257a35895df844a9787674
Yash Chauhan <yashc9411407582@gmail.com>
2025-02-03 18:52:43 +0530

    remove the extra package

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { TextField, Button, InputAdornment, Typography } from "@mui/material";
 import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
 import IconButton from "@mui/material/IconButton";
-import { use } from "react";
+
 
 const Register = () => {
   const [showPassword, setShowPassword] = useState("false");
